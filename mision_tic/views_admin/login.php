f82b8499585daa5a460b0133fad5536764884bf6
liux-soft <112289157+leonard-soft@users.noreply.github.com>
2022-09-14 09:48:04 -0500

    Add files via upload

---
+++ mision_tic/views_admin/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<script src="https://kit.fontawesome.com/4fc40aa7f8.js" crossorigin="anonymous"></script>
+	<link rel="shortcut icon" type="image/x-icon" href="../icons/logo1.png">
+	<title>LOGIN</title>
+</head>
+<body>
+    <!-- LOGIN -->
+    <div class="cont">
+        <h1>LOGIN</h1>
+    	<form method="post" action="index.php">
+    		<input type="text" name="name" placeholder="user">
+    		<br>
+    		<br>
+    		<input type="password" name="password" placeholder="password">
+    		<br>
+    		<br>
+    		<br>
+    		<button type="submit">iniciar sesion</button>
+    		<br>
+    		<br>
+    	</form>
+    </div>
+
+</body>
+</html>
+
+
